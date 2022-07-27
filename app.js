0a821115d016a01245ae21ce186bd0ea6c8cf3c1
Chienta <taducchiennb01@gmail.com>
2022-07-27 10:54:04 +0700

    add cors

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const proposalRouter = require("./routers/proposalRouter");
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api/v1/proposal", proposalRouter);
 
 module.exports = app;
